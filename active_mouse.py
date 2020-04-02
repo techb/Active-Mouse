9242dbee947cfd5dcb8cc7ee23dfe0d6c78a4366
kbcarte <kbcarte@gmail.com>
2020-04-02 18:14:09 -0400

    made it loop forever

--- active_mouse.py
+++ active_mouse.py
@@ -2,7 +2,7 @@ import ctypes
 import time
 
 print("Mouse is active every 10 minutes. Closing this window will stop active_mouse.exe")
-for i in range(500):
+while True:
     ctypes.windll.user32.SetCursorPos(200,200)
     time.sleep(600)
     ctypes.windll.user32.SetCursorPos(100,100)
